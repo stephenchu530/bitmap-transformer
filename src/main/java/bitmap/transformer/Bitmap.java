f9603194806134829fd44cde18dc3afa8ccce436
STEPHEN CHU <stephen.chu530@gmail.com>
2019-05-31 10:25:41 -0700

    feat: completed constructor

--- src/main/java/bitmap/transformer/Bitmap.java
+++ src/main/java/bitmap/transformer/Bitmap.java
@@ -8,8 +8,16 @@ import java.io.IOException;
 public class Bitmap {
     // Source code taken from SO:
     // https://stackoverflow.com/questions/16475482/how-can-i-load-a-bitmap-image-and-manipulate-individual-pixels
+
+    int height;
+    int width;
+    BufferedImage imageData;
+
+    // Constructor
     public Bitmap (BufferedImage img) {
-        
+        this.height = img.getHeight();
+        this.width = img.getWidth();
+        this.imageData = img;
     }
 
 }
